605485081d8d21f52f38c5cb17c8780b0b7f4c5b
Javierfg86 <Javierfg86@outlook.es>
2022-05-24 01:36:08 +0200

    Add character profiles

--- js/rey.js
+++ js/rey.js
@@ -2,7 +2,7 @@ import { Personaje } from './Personaje.js';
 
 export class Rey extends Personaje {
     constructor(nombre, familia, edad, estado, añosReinado) {
-        super(nombre, edad, familia, estado);
+        super(nombre, familia, edad, estado);
         this.añosDeReinado = añosReinado;
     }
     comunication = 'Vais a morir todos';
